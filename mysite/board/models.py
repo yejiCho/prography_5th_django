3fd1e90060fc0e0a3c1b98fd9a77ffdce58ffe1e
yejiCho <51953636+yejiCho@users.noreply.github.com>
2019-09-06 22:39:51 +0900

    s

--- mysite/board/models.py
+++ mysite/board/models.py
@@ -13,6 +13,8 @@ class Post(models.Model):       #게시글
         return self.title
 
 class Comment(models.Model):        #댓글
+    # foreignkey사용시 on_delete옵션 필수 상대 테이블의 레코드가 삭제 될때 본 테이블에서의 동작 지정
+    # CASCADE 'board.post'테이블의 특정 레코드가 삭제되면, 그 레코드에 연결된 Comment테이블의 레코드 삭제
     post = models.ForeignKey('board.Post', related_name='comments', on_delete=models.CASCADE)
     c_author = models.CharField(max_length=100, default="익명")
     c_content = models.TextField(default='')
